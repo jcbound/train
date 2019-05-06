81aa415255c20bb3062ac3f796b70d4544ecc539
jcbound <784538725@qq.com>
2019-05-06 17:01:30 +0800

    留言功能增加

--- js/message.js
+++ js/message.js
@@ -13,20 +13,59 @@ AV.init({
 // }).then(function(object) {
 //   alert('LeanCloud Rocks!');
 // })
+//输出数据
+var query = new AV.Query('Mssage');
+  query.find()
+  .then(function (mssages) {
+    console.log(mssages)
+    console.log('12')
+    console.log(mssages[0].attributes)
+    console.log(mssages[1].attributes)
+    let array = mssages.map((item)=> item.attributes)
+    array.forEach((item)=>{
 
+      let li =document.createElement('li')
+
+      li.innerText = `${item.name}: ${item.content}`
+
+      let mssageList = document.querySelector('#mssageList')
+      console.log(mssageList)
+      mssageList.appendChild(li)
+    })
+    // 更新成功
+  }, function (error) {
+    alert('提交失败，请改天再来留言')// 异常处理
+  })
+  
+
+//存入数据
 let myForm = document.querySelector("#postMessageForm")
 console.log(myForm)
 myForm.addEventListener("submit",function(e){
   e.preventDefault()
-  let content = myForm.querySelector("input[name=content]").Value
+  let content = myForm.querySelector("input[name=content]").value
+  let name = myForm.querySelector("input[name=name]").value
   console.log(content)
-  console.log("ceshi")
-  var Message = AV.Object.extend("Message");
-  var message = new Message();
-  message.save({
+
+  var Mssage = AV.Object.extend("Mssage")
+  var mssage = new Mssage()
+  console.log(mssage)
+  mssage.save({
+    'name':name, 
     'content':content
   }).then(function(object){
     console.log("存入成功")
+    let li =document.createElement('li')
+
+      li.innerText = `${object.attributes.name}: ${object.attributes.content}`
+      
+      let mssageList = document.querySelector('#mssageList')
+      console.log(mssageList)
+      mssageList.appendChild(li)
+      myForm.querySelector("input[name=content]").value = ""
+      myForm.querySelector("input[name=name]").value = ""
     console.log(object)
   })
+
 })
+//输出数据
